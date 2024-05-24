9e5ed534ad83aa5f2590189cd0d3a8992135e167
itc-s23019 <s23019@std.it-college.ac.jp>
2024-05-24 10:43:12 +0900

    第48回問4(22)

---
+++ d48Q22kadai.java
@@ -0,0 +1,18 @@
+// s23019
+// d48Q22のコードをコンパイルエラーしないように修正
+
+
+
+
+enum Connection {OFFLINE, ONLINE}
+
+class d48Q22 {
+		public static void main(String[] args){
+				Connection s = Connection.OFFLINE;
+				if (s == Connection.OFFLINE){
+				    s = Connection.ONLINE;
+				}
+				System.out.println(s);
+		}
+}
+
