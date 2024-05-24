64f9ca73fe112d11b4b35cb495105a6925d26b8f
itc-s23019 <s23019@std.it-college.ac.jp>
2024-05-24 11:19:15 +0900

    第48回問4(23)

---
+++ d48Q23kadai.java
@@ -0,0 +1,19 @@
+// s23019
+// コンパイルエラー
+
+
+
+class d48Q23kadai {
+	public static void main(String[] args) {
+		int i = 1;
+		for (;;) {
+			i = i + 3;
+			if (i < 10) {
+				System.out.println(i);
+				continue;
+			} else {
+				break;
+			}
+		}
+	}
+}
