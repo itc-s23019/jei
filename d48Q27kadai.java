911cc4e8505439a034abb36ae71cba969410442f
itc-s23019 <s23019@std.it-college.ac.jp>
2024-05-24 11:52:25 +0900

    第48回問4(27)

---
+++ d48Q27kadai.java
@@ -0,0 +1,14 @@
+// s23019
+// 無限ループ
+
+
+
+class d48Q27kadai {
+  public static void main(String[] args) {
+    int i = 0;
+    while (i <= 6) {
+      System.out.println(i);
+      i++;
+    }
+  }
+}
