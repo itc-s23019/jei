d14a332f1082ecd60acf8b0bfc15d7563a1a934f
itc-s23019 <s23019@std.it-college.ac.jp>
2024-05-31 10:29:53 +0900

    第48回問5

--- d48_javac/d48Q5kadai.java
+++ d48_javac/d48Q5kadai.java
@@ -1,27 +1,34 @@
 // s23019
-// 拡張for文を使用する
+// コマンドラインで指定された、複数の整数を受け取り、昇順に並べ替えて出力するプログラム
 
 
+class d48Q5kadai {
+	public static void main(String[] args) {
+		int[] numbers = new int[args.length];
 
-public class d48Q5kadai {
-  public static void main(String[] args) {
-    int[] arr = {11, 22, 33, 44, 55};
-    int n = arr.length;
+		for (int i = 0; i < args.length; i++) {
+			numbers[i] = Integer.parseInt(args[i]);
+		}
 
-    for (int i = 0; i < n - 1; i++) {
-      int maxIndex = i;
-      for (int j = i + 1; j < n; j++) {
-        if (arr[j] > arr[maxIndex]) {
-          maxIndex = j;
-        }
-        int tmp = arr[maxIndex];
-        arr[maxIndex] = arr[i];
-        arr[i] = tmp;
-      }
-    }
-    for (int num : arr) {
-      System.out.println(num);
-    }
-  }
+		for (int nextMinIndex = 0; nextMinIndex < numbers.length - 1; nextMinIndex++) {
+			int minIndex = nextMinIndex;
+			int minValue = numbers[nextMinIndex];
+			for (int checkIndex = nextMinIndex
+					+ 1; checkIndex < numbers.length; checkIndex++) {
+				if (numbers[checkIndex] < minValue) {
+					minIndex = checkIndex;
+					minValue = numbers[minIndex];
+				}
+			}
+
+			int t;
+			t = numbers[nextMinIndex] = numbers[minIndex];
+			numbers[nextMinIndex] = numbers[minIndex];
+			numbers[minIndex] = t;
+		}
+
+		for (int n : numbers) {
+			System.out.println(n + " ");
+		}
+	}
 }
-
