f11a1ec15f1fca4d3ec1065ed99a031279e8c374
itc-s23019 <s23019@std.it-college.ac.jp>
2024-08-16 12:10:56 +0900

    d52Q2kadai

---
+++ d52_java/d52Q2kadai.java
@@ -0,0 +1,20 @@
+class Temper {
+    private double temperature; // 温度
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+}
+
+public class d52Q2kadai {
+    public static void main(String[] args) {
+        Temper t = new Temper();
+        t.setTemperature(17.2);
+
+        System.out.println(t.getTemperature());
+    }
+}
